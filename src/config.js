8880334bd8055586ef73f706aa8ba5da0d78dfdd
ymolerovidatec <81310382+ymolerovidatec@users.noreply.github.com>
2021-11-21 16:41:15 -0300

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const arrUrls = [
+  "https://95bfcd06vh.execute-api.us-west-2.amazonaws.com/prod/v1/", 
+  'https://lcymlsspia.execute-api.us-east-1.amazonaws.com/dev/pdf/',
+  'https://yu20kqqsg8.execute-api.us-east-1.amazonaws.com/production/pdf/'
+];
