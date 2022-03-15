f3573a66a1717ead0abb97b683ffff4b411cc538
FernandoVelcic <fvelcic@gmail.com>
2022-03-15 17:15:47 -0300

    Add juanamanso url to allow encryt book requests

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export const arrUrls = [
   "https://95bfcd06vh.execute-api.us-west-2.amazonaws.com/prod/v1/books/", 
   'https://lcymlsspia.execute-api.us-east-1.amazonaws.com/dev/pdf/',
-  'https://yu20kqqsg8.execute-api.us-east-1.amazonaws.com/production/pdf/'
+  'https://yu20kqqsg8.execute-api.us-east-1.amazonaws.com/production/pdf/',
+  'https://execute-api.juanamanso.edu.ar/prod/v1/books/'
 ];
