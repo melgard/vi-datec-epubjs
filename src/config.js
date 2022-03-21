c73cb71cfc51947b634ef9d4b27506986bf93057
osmelmata <102065201+osmelmata@users.noreply.github.com>
2022-03-21 15:07:55 -0300

    Replace juanamanso URL by conectarigualdad domain

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ export const arrUrls = [
   "https://95bfcd06vh.execute-api.us-west-2.amazonaws.com/prod/v1/books/", 
   'https://lcymlsspia.execute-api.us-east-1.amazonaws.com/dev/pdf/',
   'https://yu20kqqsg8.execute-api.us-east-1.amazonaws.com/production/pdf/',
-  'https://execute-api.juanamanso.edu.ar/prod/v1/books/'
+  'https://execute-api.conectarigualdad.edu.ar/prod/v1/books/'
 ];
